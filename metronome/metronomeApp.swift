7eddbb0233d0d2f03f4cec17e4324417b554f2ad
Miroslav Juhy Juhos <miroslav.juhos@semantic-visions.com>
2024-03-25 17:00:13 +0100

    Initial Commit

---
+++ metronome/metronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  metronomeApp.swift
+//  metronome
+//
+//  Created by Miroslav Juhos on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct metronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
